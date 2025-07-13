9f188f16c58058896b9c0537de2c3c96771e03e3
Muhammed Rajab <muhammedrajab101@gmail.com>
2025-07-13 17:15:05 +0530

    added way to get public and private key from command line args

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <cstdlib>
 #include <iostream>
+#include <string>
 
 int main(int argc, char **argv) {
 
+  if (argc < 3) {
+    std::cerr << "Usage: " << argv[0]
+              << " <private_key.pem> <public_key.pem>\n";
+    return EXIT_FAILURE;
+  }
+
+  std::string private_key_path = argv[1];
+  std::string public_key_path = argv[2];
+
+  std::cout << "private key is at \"" << private_key_path << "\"" << std::endl;
+  std::cout << "public key is at \"" << public_key_path << "\"" << std::endl;
+
   std::cout << "Hello, P2PEE" << std::endl;
+
+  // TODO: load and use them keys;
   return EXIT_SUCCESS;
 }
