908ae1bae621d00dd662850d35735d772771f2a5
Muhammed Rajab <muhammedrajab101@gmail.com>
2025-07-13 17:31:55 +0530

    implemented a way to load private and public key for testing purposes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
+#include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <string>
+
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include <openssl/rsa.h>
+
+EVP_PKEY *load_private_key(const std::string &path) {
+  FILE *fp = fopen(path.c_str(), "r");
+  if (!fp) {
+    std::cerr << "failed to open private key file: " << path << std::endl;
+    return nullptr;
+  }
+
+  EVP_PKEY *pkey = PEM_read_PrivateKey(fp, nullptr, nullptr, nullptr);
+  fclose(fp);
+
+  if (!pkey) {
+    std::cerr << "failed to load private key: path: '" << path << "'"
+              << std::endl;
+    ERR_print_errors_fp(stderr);
+    return nullptr;
+  }
+
+  return pkey;
+}
+
+EVP_PKEY *load_public_key(const std::string &path) {
+  FILE *fp = fopen(path.c_str(), "r");
+  if (!fp) {
+    std::cerr << "failed to open public key file: " << path << std::endl;
+    return nullptr;
+  }
+
+  EVP_PKEY *pkey = PEM_read_PUBKEY(fp, nullptr, nullptr, nullptr);
+  fclose(fp);
+
+  if (!pkey) {
+    std::cerr << "failed to load public key: path: '" << path << "'"
+              << std::endl;
+
+    ERR_print_errors_fp(stderr);
+    return nullptr;
+  }
+
+  return pkey;
+}
 
 int main(int argc, char **argv) {
 
